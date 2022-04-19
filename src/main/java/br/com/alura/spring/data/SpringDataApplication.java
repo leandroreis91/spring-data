6ff7554603d90d3d3ae0247e2f593b74229ee76c
Leandro Reis <leandro.reis@outlook.com>
2022-04-18 21:32:29 -0300

    Criação do primeiro repository.

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,13 +1,32 @@
 package br.com.alura.spring.data;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.spring.data.orm.Cargo;
+import br.com.alura.spring.data.repository.CargoRepository;
+
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
+
+	
+	private final CargoRepository cargoRepository;
+	
+	
+	public SpringDataApplication(CargoRepository cargoRepository) {
+		this.cargoRepository = cargoRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		 Cargo cargo = new Cargo();
+		 cargo.setDescricao("Desenvolvedor de Software");
+		 cargoRepository.save(cargo);
 	}
 
 }
